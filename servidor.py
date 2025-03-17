7d714305e8e7c6164208b78c9a393000e8ed045c
diegogetafe <diego.jimenez@getafe.com>
2025-03-17 16:49:20 +0100

    Corrección: mantener servidor activo en Railway

--- servidor.py
+++ servidor.py
@@ -58,4 +58,5 @@ def generar_y_subir():
 
 # 🔹 SERVIDOR PARA PRODUCCIÓN EN RAILWAY
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8000, debug=False)
+
